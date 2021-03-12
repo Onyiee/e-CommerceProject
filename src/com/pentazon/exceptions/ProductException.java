3518f76e9d50ea85acf4af4b53617d49e1a54ba8
Onyiee <elizabethonyemowo.agbo@gmail.com>
2021-03-12 16:01:26 +0100

    exceptions

---
+++ src/com/pentazon/exceptions/ProductException.java
@@ -0,0 +1,18 @@
+package com.pentazon.exceptions;
+
+public class ProductException extends Exception {
+    public ProductException() {
+    }
+
+    public ProductException(String message) {
+        super(message);
+    }
+
+    public ProductException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ProductException(Throwable cause) {
+        super(cause);
+    }
+}
